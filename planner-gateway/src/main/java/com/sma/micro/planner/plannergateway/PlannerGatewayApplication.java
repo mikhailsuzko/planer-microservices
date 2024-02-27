2e022c7aed2993f76f0eea2a2b286c052ffd1e04
msuzko <msuzko@gmail.com>
2024-02-27 09:34:56 -0600

    Added new module api-gateway

---
+++ planner-gateway/src/main/java/com/sma/micro/planner/plannergateway/PlannerGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sma.micro.planner.plannergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlannerGatewayApplication.class, args);
+    }
+
+}
